f4f903e2c52437ab51813f09afd5bf51fcb668c3
Yureeeeec <nalbandyan.yura1011@gmail.com>
2023-11-11 14:23:36 +0300

    Changed the parameters of fields and models (done)

--- Warehouse_of_goods/Inventory/admin.py
+++ Warehouse_of_goods/Inventory/admin.py
@@ -1,9 +1,23 @@
 from django.contrib import admin
 from .models import Room, Shelf, Client, Product
 
-admin.site.register(Room)
-admin.site.register(Shelf)
-admin.site.register(Client)
-admin.site.register(Product)
+class RoomAdmin(admin.ModelAdmin):
+    list_display = ('name', 'volume')
+
+class ShelfAdmin(admin.ModelAdmin):
+    list_display = ('number', 'room', 'storage_capacity', 'height', 'width', 'length', 'max_load')
+    list_filter = ('room',)
+
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('name', 'bank_details')
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('height', 'width', 'length', 'weight', 'arrival_date', 'contract_number', 'client', 'contract_end_date', 'shelf', 'position')
+    list_filter = ('client', 'shelf__room')
+
+admin.site.register(Room, RoomAdmin)
+admin.site.register(Shelf, ShelfAdmin)
+admin.site.register(Client, ClientAdmin)
+admin.site.register(Product, ProductAdmin)
 
 # Register your models here.
--- Warehouse_of_goods/Inventory/models.py
+++ Warehouse_of_goods/Inventory/models.py
@@ -1,45 +1,60 @@
 from django.db import models
 
 class Room(models.Model):
-    name = models.CharField(max_length=100)
-    volume = models.DecimalField(max_digits=10, decimal_places=2)
+    name = models.CharField(max_length=100, verbose_name='Название')
+    volume = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Полезный объем')
 
     def __str__(self):
         return self.name
 
+    class Meta:
+        verbose_name_plural = 'Помещение'
+        verbose_name = 'Помещение'
+
 class Shelf(models.Model):
-    number = models.IntegerField()
-    room = models.ForeignKey(Room, on_delete=models.CASCADE)
-    storage_capacity = models.IntegerField()
-    height = models.DecimalField(max_digits=5, decimal_places=2)
-    width = models.DecimalField(max_digits=5, decimal_places=2)
-    length = models.DecimalField(max_digits=5, decimal_places=2)
-    max_load = models.DecimalField(max_digits=10, decimal_places=2)
+    number = models.IntegerField(verbose_name='Номер')
+    room = models.ForeignKey(Room, on_delete=models.CASCADE, verbose_name='Помещение')
+    storage_capacity = models.IntegerField(verbose_name='Количество мест для хранения')
+    height = models.DecimalField(max_digits=5, decimal_places=2, verbose_name='Высота')
+    width = models.DecimalField(max_digits=5, decimal_places=2, verbose_name='Ширина')
+    length = models.DecimalField(max_digits=5, decimal_places=2, verbose_name='Длина')
+    max_load = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Максимальная нагрузка')
 
     def __str__(self):
         return f"Shelf {self.number}"
+    
+    class Meta:
+        verbose_name_plural = 'Стеллажи'
+        verbose_name = 'Стеллаж'
 
 class Client(models.Model):
-    name = models.CharField(max_length=100)
-    bank_details = models.TextField()
+    name = models.CharField(max_length=100, verbose_name='Название юридического лица')
+    bank_details = models.TextField(verbose_name='Банковские реквизиты')
 
     def __str__(self):
         return self.name
+    
+    class Meta:
+        verbose_name_plural = 'Клиент'
+        verbose_name = 'Клиента'
 
 class Product(models.Model):
-    height = models.DecimalField(max_digits=5, decimal_places=2)
-    width = models.DecimalField(max_digits=5, decimal_places=2)
-    length = models.DecimalField(max_digits=5, decimal_places=2)
-    weight = models.DecimalField(max_digits=10, decimal_places=2)
-    arrival_date = models.DateField()
-    contract_number = models.CharField(max_length=100)
-    client = models.ForeignKey(Client, on_delete=models.CASCADE)
-    contract_end_date = models.DateField()
-    shelf = models.ForeignKey(Shelf, on_delete=models.CASCADE)
-    position = models.IntegerField()
+    height = models.DecimalField(max_digits=5, decimal_places=2, verbose_name='Высота')
+    width = models.DecimalField(max_digits=5, decimal_places=2, verbose_name='Ширина')
+    length = models.DecimalField(max_digits=5, decimal_places=2, verbose_name='Длина')
+    weight = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Вес')
+    arrival_date = models.DateField(verbose_name='Дата поступления')
+    contract_number = models.CharField(max_length=100, verbose_name='Номер договора')
+    client = models.ForeignKey(Client, on_delete=models.CASCADE, verbose_name='Клиент')
+    contract_end_date = models.DateField(verbose_name='Дата окончания договора')
+    shelf = models.ForeignKey(Shelf, on_delete=models.CASCADE, verbose_name='Стеллаж')
+    position = models.IntegerField(verbose_name='Позиция размещения на стеллаже')
 
     def __str__(self):
         return f"Product {self.id}"
 
+    class Meta:
+        verbose_name_plural = 'Товар'
+        verbose_name = 'Товар'
 
 # Create your models here.
